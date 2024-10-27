21d08b505e19d69cb5077393332d722f92b38421
Fernando Vicente <fernandestrade006@gmail.com>
2024-10-28 00:25:19 +0100

    ediatado tudo... pode dar segmento, ja to em outra fase

--- src/components/arquivos/arquivos.js
+++ src/components/arquivos/arquivos.js
@@ -8,8 +8,8 @@ export default function arquivosProcessos(){
 
   <div class="top-bar-arq">
     <div class="btn-group-custom">
-      <button class="btn btn-outline-primary"><i class="bi bi-box-arrow-in-down"></i> Emprestar</button>
-      <button class="btn btn-outline-secondary"><i class="bi bi-box-arrow-up"></i> Devolver</button>
+      <button class="btn btn-outline-primary"> <i class="bi bi-box-arrow-up"></i> Emprestar</button>
+      <button class="btn btn-outline-secondary"><i class="bi bi-box-arrow-in-down"></i>Devolver</button>
       <button class="btn btn-outline-success"><i class="bi bi-journal-plus"></i> Novo Processo</button>
     </div>
 
@@ -24,7 +24,506 @@ export default function arquivosProcessos(){
   </div>
 
   <div class="container-arquivos-tab">
-    
+    <table>
+      <thead>
+        <tr>
+          <th>Processo nº</th>
+          <th>Nome do Cliente / Empresa</th>
+          <th>DU nº</th>
+          <th>Descrição da Mercadoria</th>
+          <th>Data</th>
+          <th>Situação</th>
+          <th>Tempo</th>
+          <th>Arquivos</th>
+          <th>Processo nº</th>
+          <th>Nome do Cliente / Empresa</th>
+          <th>DU nº</th>
+          <th>Acões</th>
+        </tr>
+      </thead>
+      <tbody>
+         <tr>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td class="acoes">
+            <button class="btn-acao imprimir" title="Imprimir">
+              <i class="bi bi-printer"></i>
+            </button>
+            <button class="btn-acao editar" title="Editar">
+              <i class="bi bi-pencil-square"></i>
+            </button>
+            <button class="btn-acao excluir" title="Excluir">
+              <i class="bi bi-trash3"></i>
+            </button>
+            </td>
+        </tr>
+        <tr>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td class="acoes">
+            <button class="btn-acao imprimir" title="Imprimir">
+              <i class="bi bi-printer"></i>
+            </button>
+            <button class="btn-acao editar" title="Editar">
+              <i class="bi bi-pencil-square"></i>
+            </button>
+            <button class="btn-acao excluir" title="Excluir">
+              <i class="bi bi-trash3"></i>
+            </button>
+            </td>
+        </tr>
+        <tr>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td class="acoes">
+            <button class="btn-acao imprimir" title="Imprimir">
+              <i class="bi bi-printer"></i>
+            </button>
+            <button class="btn-acao editar" title="Editar">
+              <i class="bi bi-pencil-square"></i>
+            </button>
+            <button class="btn-acao excluir" title="Excluir">
+              <i class="bi bi-trash3"></i>
+            </button>
+            </td>
+        </tr>
+        <tr>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td class="acoes">
+            <button class="btn-acao imprimir" title="Imprimir">
+              <i class="bi bi-printer"></i>
+            </button>
+            <button class="btn-acao editar" title="Editar">
+              <i class="bi bi-pencil-square"></i>
+            </button>
+            <button class="btn-acao excluir" title="Excluir">
+              <i class="bi bi-trash3"></i>
+            </button>
+            </td>
+        </tr>
+        <tr>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td class="acoes">
+            <button class="btn-acao imprimir" title="Imprimir">
+              <i class="bi bi-printer"></i>
+            </button>
+            <button class="btn-acao editar" title="Editar">
+              <i class="bi bi-pencil-square"></i>
+            </button>
+            <button class="btn-acao excluir" title="Excluir">
+              <i class="bi bi-trash3"></i>
+            </button>
+            </td>
+        </tr>
+        <tr>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td class="acoes">
+            <button class="btn-acao imprimir" title="Imprimir">
+              <i class="bi bi-printer"></i>
+            </button>
+            <button class="btn-acao editar" title="Editar">
+              <i class="bi bi-pencil-square"></i>
+            </button>
+            <button class="btn-acao excluir" title="Excluir">
+              <i class="bi bi-trash3"></i>
+            </button>
+            </td>
+        </tr>
+        <tr>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td class="acoes">
+            <button class="btn-acao imprimir" title="Imprimir">
+              <i class="bi bi-printer"></i>
+            </button>
+            <button class="btn-acao editar" title="Editar">
+              <i class="bi bi-pencil-square"></i>
+            </button>
+            <button class="btn-acao excluir" title="Excluir">
+              <i class="bi bi-trash3"></i>
+            </button>
+            </td>
+        </tr>
+        <tr>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td class="acoes">
+            <button class="btn-acao imprimir" title="Imprimir">
+              <i class="bi bi-printer"></i>
+            </button>
+            <button class="btn-acao editar" title="Editar">
+              <i class="bi bi-pencil-square"></i>
+            </button>
+            <button class="btn-acao excluir" title="Excluir">
+              <i class="bi bi-trash3"></i>
+            </button>
+            </td>
+        </tr>
+        <tr>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td class="acoes">
+            <button class="btn-acao imprimir" title="Imprimir">
+              <i class="bi bi-printer"></i>
+            </button>
+            <button class="btn-acao editar" title="Editar">
+              <i class="bi bi-pencil-square"></i>
+            </button>
+            <button class="btn-acao excluir" title="Excluir">
+              <i class="bi bi-trash3"></i>
+            </button>
+            </td>
+        </tr>
+        <tr>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td class="acoes">
+            <button class="btn-acao imprimir" title="Imprimir">
+              <i class="bi bi-printer"></i>
+            </button>
+            <button class="btn-acao editar" title="Editar">
+              <i class="bi bi-pencil-square"></i>
+            </button>
+            <button class="btn-acao excluir" title="Excluir">
+              <i class="bi bi-trash3"></i>
+            </button>
+            </td>
+        </tr>
+        <tr>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td class="acoes">
+            <button class="btn-acao imprimir" title="Imprimir">
+              <i class="bi bi-printer"></i>
+            </button>
+            <button class="btn-acao editar" title="Editar">
+              <i class="bi bi-pencil-square"></i>
+            </button>
+            <button class="btn-acao excluir" title="Excluir">
+              <i class="bi bi-trash3"></i>
+            </button>
+            </td>
+        </tr>
+        <tr>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td class="acoes">
+            <button class="btn-acao imprimir" title="Imprimir">
+              <i class="bi bi-printer"></i>
+            </button>
+            <button class="btn-acao editar" title="Editar">
+              <i class="bi bi-pencil-square"></i>
+            </button>
+            <button class="btn-acao excluir" title="Excluir">
+              <i class="bi bi-trash3"></i>
+            </button>
+            </td>
+        </tr>
+        <tr>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td class="acoes">
+            <button class="btn-acao imprimir" title="Imprimir">
+              <i class="bi bi-printer"></i>
+            </button>
+            <button class="btn-acao editar" title="Editar">
+              <i class="bi bi-pencil-square"></i>
+            </button>
+            <button class="btn-acao excluir" title="Excluir">
+              <i class="bi bi-trash3"></i>
+            </button>
+            </td>
+        </tr>
+        <tr>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td class="acoes">
+            <button class="btn-acao imprimir" title="Imprimir">
+              <i class="bi bi-printer"></i>
+            </button>
+            <button class="btn-acao editar" title="Editar">
+              <i class="bi bi-pencil-square"></i>
+            </button>
+            <button class="btn-acao excluir" title="Excluir">
+              <i class="bi bi-trash3"></i>
+            </button>
+            </td>
+        </tr>
+        <tr>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td class="acoes">
+            <button class="btn-acao imprimir" title="Imprimir">
+              <i class="bi bi-printer"></i>
+            </button>
+            <button class="btn-acao editar" title="Editar">
+              <i class="bi bi-pencil-square"></i>
+            </button>
+            <button class="btn-acao excluir" title="Excluir">
+              <i class="bi bi-trash3"></i>
+            </button>
+            </td>
+        </tr>
+        <tr>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td class="acoes">
+            <button class="btn-acao imprimir" title="Imprimir">
+              <i class="bi bi-printer"></i>
+            </button>
+            <button class="btn-acao editar" title="Editar">
+              <i class="bi bi-pencil-square"></i>
+            </button>
+            <button class="btn-acao excluir" title="Excluir">
+              <i class="bi bi-trash3"></i>
+            </button>
+            </td>
+        </tr>
+        <tr>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td class="acoes">
+            <button class="btn-acao imprimir" title="Imprimir">
+              <i class="bi bi-printer"></i>
+            </button>
+            <button class="btn-acao editar" title="Editar">
+              <i class="bi bi-pencil-square"></i>
+            </button>
+            <button class="btn-acao excluir" title="Excluir">
+              <i class="bi bi-trash3"></i>
+            </button>
+            </td>
+        </tr>
+        <tr>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td class="acoes">
+            <button class="btn-acao imprimir" title="Imprimir">
+              <i class="bi bi-printer"></i>
+            </button>
+            <button class="btn-acao editar" title="Editar">
+              <i class="bi bi-pencil-square"></i>
+            </button>
+            <button class="btn-acao excluir" title="Excluir">
+              <i class="bi bi-trash3"></i>
+            </button>
+            </td>
+        </tr>
+        <tr>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td class="acoes">
+            <button class="btn-acao imprimir" title="Imprimir">
+              <i class="bi bi-printer"></i>
+            </button>
+            <button class="btn-acao editar" title="Editar">
+              <i class="bi bi-pencil-square"></i>
+            </button>
+            <button class="btn-acao excluir" title="Excluir">
+              <i class="bi bi-trash3"></i>
+            </button>
+            </td>
+        </tr>
+        <tr>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td>CCA/DD/789</td>
+          <td class="acoes">
+            <button class="btn-acao imprimir" title="Imprimir">
+              <i class="bi bi-printer"></i>
+            </button>
+            <button class="btn-acao editar" title="Editar">
+              <i class="bi bi-pencil-square"></i>
+            </button>
+            <button class="btn-acao excluir" title="Excluir">
+              <i class="bi bi-trash3"></i>
+            </button>
+            </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </div>
     `;
