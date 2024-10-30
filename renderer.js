6300ce3aa46672812e00da22549bd0c48b035394
Fernando Vicente <fernandestrade006@gmail.com>
2024-10-30 13:25:45 +0100

    nada de mais

--- renderer.js
+++ renderer.js
@@ -1,7 +1,7 @@
 import { Navigate, Route } from "./src/Routes.js";
 export const mainContainer = document.querySelector("#mainContainer");
 window.addEventListener("DOMContentLoaded", () => {
-  Navigate("/paises");
+  Navigate("/posicoes");
   Route();
 });
 window.addEventListener("hashchange", Route);
