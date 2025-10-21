8acbf44e740570625702eca54bc40844c494444d
Fernando Vicente <fernandestrade006@gmail.com>
2025-10-21 10:46:31 +0100

    atualizacoes no renderer e no index.css

--- renderer.js
+++ renderer.js
@@ -1,7 +1,7 @@
 import { Navigate, Route } from "./src/Routes.js";
 export const mainContainer = document.querySelector("#mainContainer");
 window.addEventListener("DOMContentLoaded", () => {
-  Navigate("/login");
+  Navigate("/");
   Route();
 });
 window.addEventListener("hashchange", Route);
