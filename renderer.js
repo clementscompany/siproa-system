66e7c4f03b7ea022d4312e2fdd794a29a7bc1f81
Moises Ckemente <ajbcompany4881@gmail.com>
2025-10-21 10:54:24 +0100

    Refactor: Atualizando o Login Code

--- renderer.js
+++ renderer.js
@@ -1,7 +1,7 @@
 import { Navigate, Route } from "./src/Routes.js";
 export const mainContainer = document.querySelector("#mainContainer");
 window.addEventListener("DOMContentLoaded", () => {
-  Navigate("/");
+  Navigate("/login"); 
   Route();
 });
 window.addEventListener("hashchange", Route);
--- src/pages/loginPage/LoginPage.js
+++ src/pages/loginPage/LoginPage.js
@@ -3,9 +3,10 @@ import Login from "../../components/wellcome/login.js"
 class LoginPage {
   constructor(mainContainer) {
     this.mainContainer = mainContainer;
+    this.Init(mainContainer);
   }
 
-  Init(mainContainer){
+  Init(mainContainer) {
     mainContainer.innerHTML = Login();
   }
 }
