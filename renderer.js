83d57961b1d99bf8d1441b4cc9739add7fc1bc51
Moises Ckemente <ajbcompany4881@gmail.com>
2025-10-28 16:22:52 +0100

    render

--- renderer.js
+++ renderer.js
@@ -1,7 +1,7 @@
 import { Navigate, Route } from "./src/Routes.js";
 export const mainContainer = document.querySelector("#mainContainer");
 window.addEventListener("DOMContentLoaded", () => {
-  Navigate("/arquivos");
+  Navigate("/wellcome");
   Route();
 });
 window.addEventListener("hashchange", Route);
