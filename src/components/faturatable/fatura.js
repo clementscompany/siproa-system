0c3dd937dc61bc2ecb555ad528b82a916dd4976a
Fernando Vicente <fernandestrade006@gmail.com>
2025-11-03 12:56:22 +0100

    atualizado

--- src/components/faturatable/fatura.js
+++ src/components/faturatable/fatura.js
@@ -6,59 +6,66 @@ export default function fatura(){
             <h3>JMX - José Mayamona Xingui</h3>
             <p>Despachante Oficial • Cédula nº 01068 Série D</p>
             <h4>Direcção Regional das Alfândegas de Cabinda</h4>
-        </header>
+            </header>
+            
+            <!-- DADOS PRINCIPAIS -->
+            <table class="dados-principais">
+                <tr>
+                    <th>Cliente/Empresa</th>
+                    <td style="font-weight:600;">Global Trading Lda</td>
+                    <th>Req. Fundo Nº</th>
+                    <td style="font-weight:600;">RF-8812</td>
+                </tr>
 
-        <!-- DADOS PRINCIPAIS -->
-        <table class="dados-principais">
-            <tr>
-                <th>Data de Entrada</th>
-                <td>01/11/2025</td>
-                <th>C/Marca Nº</th>
-                <td>CM-2025-044</td>
-            </tr>
-            <tr>
-                <th>B.L. Nº</th>
-                <td>BL-3399</td>
-                <th>Via</th>
-                <td>Marítima</td>
-            </tr>
-            <tr>
-                <th>Origem</th>
-                <td>China</td>
-                <th>Fob</th>
-                <td>12 000,00</td>
-            </tr>
-            <tr>
-                <th>Frete</th>
-                <td>3 500,00</td>
-                <th>Seguro</th>
-                <td>500,00</td>
-            </tr>
-            <tr>
-                <th>CIF</th>
-                <td>16 000,00</td>
-                <th>Cliente/Empresa</th>
-                <td>Global Trading Lda</td>
-            </tr>
-            <tr>
-                <th>Req. Fundo Nº</th>
-                <td>RF-8812</td>
-                <th>Moeda</th>
-                <td>USD</td>
-            </tr>
-            <tr>
-                <th>Câmbio</th>
-                <td>840,00</td>
-                <th>Valor Aduaneiro</th>
-                <td>Kz 13 440 000,00</td>
-            </tr>
-            <tr>
-                <th>Factura Nº</th>
-                <td>FT-7791</td>
-                <th>D.U. Nº</th>
-                <td>DU-5549</td>
-            </tr>
-        </table>
+                <tr>
+                    <th>B.L. Nº</th>
+                    <td>BL-3399</td>
+                    <th>C/Marca Nº</th>
+                    <td>CM-2025-044</td>
+                </tr>
+
+                <tr>
+                    <th>Data de Entrada</th>
+                    <td>01/11/2025</td>
+                    <th>Via</th>
+                    <td>Marítima</td>
+                </tr>
+
+                <tr>
+                    <th>Fob</th>
+                    <td>12 000,00</td>
+                    <th>Moeda</th>
+                    <td>USD</td>
+                </tr>
+             
+                <tr>
+                    <th>Frete</th>
+                    <td>3 500,00</td>
+                    <th>Câmbio</th>
+                    <td>840,00</td>
+                </tr>
+
+                <tr>
+                    <th>Seguro</th>
+                    <td>500,00</td>
+                    <th>Valor Aduaneiro</th>
+                    <td>Kz 13 440 000,00</td>
+                </tr>
+
+                <tr>
+                    <th>CIF</th>
+                    <td>16 000,00</td>
+                    <th>Factura Nº</th>
+                    <td>FT-7791</td>
+                </tr>
+
+                <tr>
+                    <th>Origem</th>
+                    <td>China</td>
+                    <th>D.U. Nº</th>
+                    <td>DU-5549</td>
+                </tr>
+            </table>
 
         <h4>Designação da Mercadoria:</h4>
         <p>Motores Industriais - Peças sobressalentes</p>
@@ -131,17 +138,22 @@ export default function fatura(){
                     <td>1 844 428,00</td>
                 </tr>
                 <tr>
-                    <td>8. Veterinário/Saúde</td>
+                    <td>8. EP 14</td>
+                    <td>Kz</td>
+                    <td>1 844 428,00</td>
+                </tr>
+                <tr>
+                    <td>9. Veterinário/Saúde</td>
                     <td>Kz</td>
                     <td>0,00</td>
                 </tr>
                 <tr>
-                    <td>9. Validação do BL</td>
+                    <td>10. Validação do BL</td>
                     <td>Kz</td>
                     <td>1 003 380,39</td>
                 </tr>
                 <tr>
-                    <td>10. Deslocação e Acompanhamento</td>
+                    <td>11. Deslocação e Acompanhamento</td>
                     <td>Kz</td>
                     <td>140 000,00</td>
                 </tr>
@@ -165,27 +177,27 @@ export default function fatura(){
             </thead>
             <tbody>
                 <tr>
-                    <td>11. Honorário</td>
+                    <td>12. Honorário</td>
                     <td>Kz</td>
                     <td>1 000 000,00</td>
                 </tr>
                 <tr>
-                    <td>12. Licenciamento</td>
+                    <td>13. Licenciamento</td>
                     <td>Kz</td>
                     <td>20 000,00</td>
                 </tr>
                 <tr>
-                    <td>13. Declaração de Valor</td>
+                    <td>14. Declaração de Valor</td>
                     <td>Kz</td>
                     <td>0,00</td>
                 </tr>
                 <tr>
-                    <td>14. Deslocação</td>
+                    <td>15. Deslocação</td>
                     <td>Kz</td>
                     <td>250 000,00</td>
                 </tr>
                 <tr>
-                    <td>15. Outros</td>
+                    <td>16. Outros</td>
                     <td>Kz</td>
                     <td>5 000,00</td>
                 </tr>
